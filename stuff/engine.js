9e74bc947ab85c67790b93b77013007abb7ce619
Jackrifleman <Jeubank100@gmail.com>
2022-07-28 00:27:10 -0700

    added basic attributes and some more names

--- stuff/engine.js
+++ stuff/engine.js
@@ -24,7 +24,9 @@ prefixes = [
             "Greaser", "1000-yard", "Jackrifleman", "Fatal", "Recycled", "Cardboard", "Eyelash",
             "Uncle", "Elongated", "Epic", "Nerd", "Christmas", "Merry", "Rodney", "Randal",
             "Gromky", "Razrez", "Toe", "Wife", "Spouse", "Husband", "Tempered", "Experimental",
-            "Mental", "Kid"
+            "Mental", "Kid", "Boy", "Salty", "Gregorian", "Hindenburg", "Prime", "Glizzy", "Eidolon",
+            "Mississippi", "Fort", "Night", "Battle", "Raw", "Milk", "Giga", "Explosive", "Boom", "Midnight",
+            ""
         ];
 
 names = [
@@ -37,8 +39,32 @@ names = [
             "Janitor", "Cleaner", "Mama", "Papa" , "Rainmaker", "Peeler", "Butcher", "Death", "Killer",
             "Basher", "Maul", "Murder", "Stare", "Jackrifleman", "Maker", "Rifle", "Attractor", "Fatality",
             "Sucker", "Catastrophe", "Shaft", "Disaster", "Rod", "Jimmy", "Lyuda", "Goofball", "Stabber",
-            "Shank"
+            "Shank", "Jabber", "Shot", "Cleaver", "Splitter", "Mangler", "Massacre", "Incident", "Prime", "Crime",
+            "Master", "Gobbler", "Disciple", "Mile", "Ally", "King", "Queen", "Night", "Pass", "Barnacle", "Maid",
+            "Boy", "Chad", "Bender", "Player", "Bomb", "Special", "Christmas"
 ];
+
+attributes = [
+        [
+            "+$PER% damage bonus", "+$PER% clip size", "+$PER% faster firing speed", "On Hit: Gain $STATUS for $INT seconds",
+            "On Hit: Gain up to +$PER health", "+$PER max health on wearer", "On Kill: Gain $STATUS for $INT seconds",
+            "On Hit: Ignites target for $INT seconds", "On Hit: Bleeds target for $INT seconds"
+        ],
+        [
+            "-$PER% damage penalty", "-$PER% clip size", "-$PER max health on wearer", "On Damage: Become $STATUS for $INT seconds",
+            "On Damage: Instantly die", "On Miss: Hit yourself. Idiot."
+        ]
+
+]
+
+statuses = [
+        [
+            "100% critical chance", "speed bonus", "mini-crits", "invulnerability"
+        ],
+        [
+            "slowed", "marked for death", "ignited", "doused in Mad Milk", "doused in Jarate"
+        ]
+]
 
 function irandom(x) {
     return Math.floor(Math.random() * x);
@@ -65,9 +91,45 @@ function generateWeapon()
     _fname = "The " + _prefix + " " + _name;
     _lvlInt = Math.floor((irandom(99)))+1//+1)/5) * 5;
     _lvl = "Level " + _lvlInt.toString() + " " + _wep;
-    _pos = "";
-    _neg = "";
+    _pos = attributeAddPos();
+    _neg = attributeAddNeg();
 
     modifyDOM(_fname,_lvl,_pos,_neg);
     
 };
+
+function getAttribute(neg) {
+    _att = attributes[neg][irandom(attributes[neg].length)];
+    _percent = Math.floor((((irandom(99)))+5)/5) * 5;
+    //console.log(_percent.toString());
+    _att = _att.replace("$PER", _percent.toString());
+    _att = _att.replace("$STATUS", statuses[neg][irandom(statuses[neg].length)]);
+    _int = Math.floor((((irandom(29)))+5)/5) * 5;
+    _att = _att.replace("$INT", _int.toString());
+
+    return _att;
+}
+
+function attributeAddPos() {
+    _cnt = irandom(2)+1;
+    _att = "";
+    for (i = 0; i < _cnt; i++) {
+        _att += getAttribute(0);
+        if (i < _cnt-1) {
+            _att += "<br>";
+        }
+    }
+    return _att;
+}
+
+function attributeAddNeg() {
+    _cnt = irandom(2);
+    _att = "";
+    for (i = 0; i < _cnt; i++) {
+        _att += getAttribute(1);
+        if (i < _cnt-1) {
+            _att += "<br>";
+        }
+    }
+    return _att;
+}
