2b6059cf62140e57ac26c10f957b073fb058d663
Jackrifleman <Jeubank100@gmail.com>
2022-07-30 23:48:23 -0700

    Update engine.js

--- stuff/engine.js
+++ stuff/engine.js
@@ -57,40 +57,50 @@ names = [
 
 attributes = [
         [
-            "+$PER% damage bonus", "+$PER% clip size", "+$PER% faster firing speed", "On Hit: Gain $STATUS for $INT seconds",
-            "On Hit: Gain up to +$PER health", "+$PER max health $ATYPE", "On Kill: Gain $STATUS for $INT seconds",
-            "On Hit: Ignites target for $INT seconds", "On Hit: Bleeds target for $INT seconds", "$PER% faster move speed $ATYPE",
-            "On Kill: +$PER% damage bonus for $INT seconds", "+$PER% more accurate", "Mini-crits airborne targets",
+            "+~PER% damage bonus", "+~PER% clip size", "+~PER% faster firing speed", "On Hit: Gain ~STATUS for ~INT seconds",
+            "On Hit: Gain up to +~PER health", "+~PER max health on wearer", "On Kill: Gain ~STATUS for ~INT seconds",
+            "On Hit: Ignites target for ~INT seconds", "On Hit: Bleeds target for ~INT seconds", "~PER% faster move speed ~ATYPE",
+            "On Kill: +~PER% damage bonus for ~INT seconds", "+~PER% more accurate", "Mini-crits airborne targets",
             "Damage increases as the user becomes injured", "Damage removes Sappers", "Imbued with an ancient power",
-            "Crits whenever it would normally mini-crit", "$PER% faster weapon switch speed $ATYPE",
-            "100% mini-crits vs burning players", "Does not require ammo", "Projectiles penetrate players",
-            "+$PER health gained per second $ATYPE", "On Hit: Damage dealt is returned as ammo", "+$PER% max ammo $ATYPE",
-            "+$PER% greater jump height $ATYPE", "+$PER% $DMG damage resistance $ATYPE", "On Kill: Gain $PER% of base health on kill",
-            "On Kill: Weapon is reloaded from reserves", "Immunity to headshots $ATYPE", "Immunity to backstabs $ATYPE", "Immunity to fall damage $ATYPE",
-            "On Hit Teammate: They gain $STATUS for $INT seconds", "On Hit: Target is $STSNEG for $INT seconds", "On Hit Teammate: Extinguishes teammates",
-            "On Miss: Gain $STATUS for $INT seconds", "100% critical chance vs wet players", "Mini-crits vs wet players",
-            "On Hit Teammate: Gain $STATUS for $INT seconds", "On Hit Teammate: You both gain $STATUS for $INT seconds", "Leave a calling card on your victims"
+            "Crits whenever it would normally mini-crit", "~PER% faster weapon switch speed ~ATYPE",
+            "Mini-crits vs burning players", "Does not require ammo", "Projectiles penetrate players",
+            "+~INT health gained per second ~ATYPE", "On Hit: Damage dealt is returned as ammo", "+~PER% max ammo on wearer",
+            "+~PER% greater jump height ~ATYPE", "+~PER% ~DMG damage resistance ~ATYPE", "On Kill: Gain ~PER% of base health on kill",
+            "On Kill: Weapon is reloaded from reserves", "Immunity to headshots ~ATYPE", "Immunity to backstabs ~ATYPE", "Immunity to fall damage ~ATYPE",
+            "On Hit Teammate: They gain ~STATUS for ~INT seconds", "On Hit: Target is ~STSNEG for ~INT seconds", "Extinguishes teammates",
+            "On Miss: Gain ~STATUS for ~INT seconds", "100% critical chance vs wet players", "Mini-crits vs wet players",
+            "On Hit Teammate: Gain ~STATUS for ~INT seconds", "On Hit Teammate: You both gain ~STATUS for ~INT seconds", "Leave a calling card on your victims",
+            "+~PER% damage bonus vs minors", "+~PER% damage bonus vs buildings", "100% critical hits vs burning players", "100% critical hits from behind",
+            "Mini-crits from behind", "Dealing ~MTRDMG damage fills the ~METER meter, alt-click to activate ~METER, and gain ~STATUS and ~STATUS for ~INT seconds"
         ],
         [
-            "-$PER% damage penalty", "-$PER% clip size", "-$PER max health $ATYPE", "On Damage: Become $STATUS for $INT seconds",
-            "On Damage: Instantly die", "On Miss: Hit yourself. Idiot.", "No random critical hits", "On Miss: Lose -$PER health",
-            "On Miss: Instantly die", "$PER% slower move speed $ATYPE", "-$PER% less accurate", "Damage decreases as the user becomes injured",
-            "-$PER health drained per second $ATYPE", "$PER% slower weapon switch speed $ATYPE", "Fires tracer rounds", "-$PER% max ammo $ATYPE",
-            "On Miss: Become $STATUS for $INT seconds", "$PER% $DMG damage vulnerability $ATYPE", "On Hit: Target gains $STSPOS for $INT seconds",
-            "On Hit Teammate: They become $STATUS for $INT seconds", "On Hit: Extinguishes target", "On Hit Teammate: You both become $STATUS for $INT seconds"
+            "-~PER% damage penalty", "-~PER% clip size", "-~PER max health on wearer", "On Damage: Become ~STATUS for ~INT seconds",
+            "On Damage: Instantly die while deployed", "On Miss: Hit yourself. Idiot.", "No random critical hits", "On Miss: Lose -~PER health",
+            "On Miss: Instantly die", "~PER% slower move speed ~ATYPE", "-~PER% less accurate", "Damage decreases as the user becomes injured",
+            "-~INT health drained per second ~ATYPE", "~PER% slower weapon switch speed ~ATYPE", "Fires tracer rounds", "-~PER% max ammo on wearer",
+            "On Miss: Become ~STATUS for ~INT seconds", "~PER% ~DMG damage vulnerability ~ATYPE", "On Hit: Target gains ~STSPOS for ~INT seconds",
+            "On Hit Teammate: They become ~STATUS for ~INT seconds", "On Hit: Extinguishes target", "On Hit Teammate: You both become ~STATUS for ~INT seconds",
+            "Accuracy decreases as the user becomes injured", "Mini-crits whenever it would normally crit", "-~PER% damage penalty vs buildings",
+            "On Hit: Hit yourself. Idiot."
         ]
 
 ]
+
+meters = ["CRIKEY", "RAGE", "GRRR", "MAYO", "HYPE"]
 
 statuses = [
         [
-            "100% critical chance", "a speed bonus", "mini-crits", "invulnerability"
+            "100% critical chance", "a speed boost", "mini-crits", "invulnerability", "+~PER% damage resistance",
+            "backstab immunity", "afterburn immunity", "headshot immunity", "+~PER% ~DMG damage resistance",
+            "+~PER max health", "~DMG damage immunity", "afterburn immunity"
         ],
         [
-            "slowed", "marked for death", "ignited", "doused in Mad Milk", "doused in Jarate",
-            "feared"
+            "slowed by ~PER%", "marked for death", "ignited", "doused in Mad Milk", "doused in Jarate",
+            "feared", "stunned", "~PER% more vulnerable to damage", "~PER% more vulnerable to ~DMG damage"
         ]
 ]
+
+
 
 damage = [
     "explosive", "bullet", "fire", "melee", "critical"
@@ -126,6 +136,7 @@ function generateWeapon()
     _class = irandom(classes.length);
     _class_name = classes[_class];
     _wep = weapons[_class][irandom(weapons[_class].length)];
+    _attribs = [[],[]];
     _prefix = prefixes[irandom(prefixes.length)];
     _name = names[irandom(names.length)];
     _fname = "The " + _prefix + " " + _name;
@@ -139,19 +150,35 @@ function generateWeapon()
 };
 
 function getAttribute(neg) {
-    _att = attributes[neg][irandom(attributes[neg].length)];
+
+    _ind = irandom(attributes[neg].length);
+    while(_attribs[neg].includes(_ind)) {
+        _ind = irandom(attributes[neg].length);
+    }
+
+    _attribs[neg].push(_ind);
+
+    _att = attributes[neg][_ind];
     _percent = Math.floor((((irandom(99)))+5)/5) * 5;
-
-    _att = _att.replace("$STATUS", statuses[neg][irandom(statuses[neg].length)]);
-    _att = _att.replace("$STSPOS", statuses[0][irandom(statuses[0].length)]);
-    _att = _att.replace("$STSNEG", statuses[1][irandom(statuses[1].length)]);
-
     _int = Math.floor((((irandom(29)))+5)/5) * 5;
-    _att = _att.replace("$DMG", damage[irandom(damage.length)]);
-    _att = _att.replace("$ATYPE", activeType[irandom(2)]);
+    _mtr = meters[irandom(meters.length)];
+    _mtrDmg = 50 * (irandom(8) + 2);
 
-    _att = _att.replace("$INT", _int.toString());
-    _att = _att.replace("$PER", _percent.toString());
+    while(_att.search("~") != -1) {
+        _att = _att.replace("~STATUS", statuses[neg][irandom(statuses[neg].length)]);
+        _att = _att.replace("~STSPOS", statuses[0][irandom(statuses[0].length)]);
+        _att = _att.replace("~STSNEG", statuses[1][irandom(statuses[1].length)]);
+
+        
+        _att = _att.replace("~METER", _mtr);
+        _att = _att.replace("~MTRDMG", _mtrDmg.toString());
+
+        _att = _att.replace("~DMG", damage[irandom(damage.length)]);
+        _att = _att.replace("~ATYPE", activeType[irandom(2)]);
+
+        _att = _att.replace("~INT", _int.toString());
+        _att = _att.replace("~PER", _percent.toString());
+    }
 
     return _att;
 }
